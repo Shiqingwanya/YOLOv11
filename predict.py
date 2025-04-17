828ef734d9b495b5009e52d9fa442b0c1bc136e2
QINGWAN <1513911506@qq.com>
2025-04-18 00:48:39 +0800

    2-VERSION

--- predict.py
+++ predict.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("my_model.pt")
 
-results = model.predict("testimg/1_video.mp4", show_boxes=True, save=True)
+results = model.predict("testimg/1.jpg", show_boxes=True, save=True)
